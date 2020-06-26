08aa642e24474506c6220785d4211b3828ddad46
Andre Ferrer <andrerferrer@hotmail.com>
2020-06-26 18:31:40 -0300

    add update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'restaurants#index'
-  resources :restaurants, only: %i[index show new create destroy] do
+  resources :restaurants, only: %i[index show new create destroy update] do
     resources :reviews, only: :create
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
